7ed115e92129fdd255a23652ea63a2acd151c1cb
Ted Metcalfe <ted.metcalfe@gmail.com>
2014-08-27 00:27:33 -0400

    added histogram of end distances

--- randomWalk.m
+++ randomWalk.m
@@ -5,7 +5,7 @@
 % The range that the direction of the motion can change with each update
 % Currently assumes 2D Cartesian (Euclidean?) space, ie, 360 degrees of
 % variation 
-rangeAngleVaries = 4;
+rangeAngleVaries = 180;
 
 % Starting location
 xStartLocation = 0;
@@ -26,8 +26,8 @@ nTrials = 1000;
 %% Random walk function
 
 % Define vectors to hold terminal xPosition and yPosition for each iTrial
-xPositionArray = 1:nTrials;
-yPositionArray = 1:nTrials;
+xEndPosition = 1:nTrials;
+yEndPosition = 1:nTrials;
 stepSizes = 1:(nTrials * nSteps);
 
 for iTrial = 1:nTrials
@@ -42,33 +42,40 @@ for iTrial = 1:nTrials
         xPosition = xPosition + (stepSize * cos(theta)); % compute x displacement
         yPosition = yPosition + (stepSize * sin(theta)); % compute y displacement 
     end 
-    xPositionArray(iTrial) = xPosition; % insert iTrial end xPosition to vector of all end positions
-    yPositionArray(iTrial) = yPosition; % insert iTrial end yPosition to vector of all end positions
+    xEndPosition(iTrial) = xPosition; % insert iTrial end xPosition to vector of all end positions
+    yEndPosition(iTrial) = yPosition; % insert iTrial end yPosition to vector of all end positions
 end
 
 %% Summary stats and scatter plot
 
-distances = sqrt(xPositionArray.^2 + yPositionArray.^2);
+distances = sqrt(xEndPosition.^2 + yEndPosition.^2);
 meanDistance = mean(distances);
 
 FigHandle = figure('color', 'w', 'Position', [100, 100, 1200, 800]); % set figure res to 1200x800
 
     % Display config summary stats
-    subplot(2,3,1); % first of 2x2 subplot
+    subplot(2,3,1); % first subplot
     str(1) = {['Steps:', num2str(nSteps),'. Trials:', num2str(nTrials),...
         '. Angles:' num2str(rangeAngleVaries), '.']};
     str(2) = {['Distance walked: M = ', num2str(meanDistance),...
         ', SD = ', num2str(std(distances))]};
-    str(3) = {['End x position: M = ', num2str(mean(xPositionArray)),...
-        ', SD = ', num2str(std(xPositionArray))]};
-    str(4) = {['End y position: M = ', num2str(mean(yPositionArray)),...
-        ', SD = ', num2str(std(yPositionArray))]};    
+    str(3) = {['End x position: M = ', num2str(mean(xEndPosition)),...
+        ', SD = ', num2str(std(xEndPosition))]};
+    str(4) = {['End y position: M = ', num2str(mean(yEndPosition)),...
+        ', SD = ', num2str(std(yEndPosition))]};    
     str(5) = {['Step size: M = ', num2str(mean(stepSizes)),', SD = ', num2str(std(stepSizes))]};
     text(0,.8,str);axis off
     
     % Scatterplot of end positions   
-    subplot(2,3,2) % second of 2x2 subplot
-    title('End positions');
+    subplot(2,3,2); % second subplot
     s = (meanDistance/nTrials) * 1000; % marker size
     c = [0 0 0]; % marker color
-    scatter(xPositionArray, yPositionArray, s, c, '.');
+    scatter(xEndPosition, yEndPosition, s, c, '.');
+    title('End positions');
+
+    % Histogram of end distances   
+    subplot(2,3,3); % third subplot
+    hist(distances);
+    h = findobj(gca,'Type','patch');
+    set(h,'FaceColor',[0 0 0],'EdgeColor','w')
+    title('End distances');
